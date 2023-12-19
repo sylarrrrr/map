b3686be01f757c5ef7895dfc8c5a885d41931b2a
klingaard <klingaard@users.noreply.github.com>
2023-12-19 15:59:18 +0000

    [ci skip] Update README.md 8b16b216de9960cacdc22d78820165ab86d6de58

---
+++ modules.js
@@ -0,0 +1,6 @@
+var modules =
+[
+    [ "Iteration Support", "group__iteration.html", "group__iteration" ],
+    [ "Logger Output Macros", "group__group1.html", "group__group1" ],
+    [ "Logger Hex Macros", "group__group2.html", "group__group2" ]
+];
